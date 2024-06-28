92d5324ef7c2a94e061402be5f1c234a3a0c5c05
fatihbey <101522275+thefatih42@users.noreply.github.com>
2024-06-28 21:20:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,66 +1,22 @@
-// Service Worker
-
-const CACHE_NAME = 'boykotdedektifi-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    // diğer dosyalarınızı buraya ekleyin
-];
-
 self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+    // Kurulum sırasında dosyaları önbelleğe ekleyin
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+        caches.open('static-cache-v1')
+            .then(cache => cache.addAll([
+                '/',
+                '/index.html',
+                'image1.png',
+                '/logo.svg'
+            ]))
     );
 });
 
 self.addEventListener('fetch', event => {
+    console.log('Service Worker fetching:', event.request.url);
+    // Öncelikle önbelleği kontrol edin, eğer önbellekte yoksa ağdan yükleyin
     event.respondWith(
         caches.match(event.request)
-            .then(response => {
-                // Önbellekte bir eşleşme var mı kontrol et
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then(
-                    response => {
-                        // Yanıt geçerli değilse önbelleğe eklemeyin
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // Yanıtın bir kopyasını önbelleğe ekleyin
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
-            })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
-
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+            .then(response => response || fetch(event.request))
     );
 });
