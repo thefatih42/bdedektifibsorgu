b2b010c1b5a254475b07236760a36fc0b1c98ff0
fatihbey <101522275+thefatih42@users.noreply.github.com>
2024-06-29 01:38:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+// Service Worker kurulum aşaması
 self.addEventListener('install', event => {
     console.log('Service Worker installing.');
     // Kurulum sırasında dosyaları önbelleğe ekleyin
@@ -6,33 +7,34 @@ self.addEventListener('install', event => {
             .then(cache => cache.addAll([
                 '/',
                 '/index.html',
-                'sw.js',
-                'image1.png',
-                'bimage/logo.svg',
-                'bimage/algida.jpg',
-                'bimage/Coca-Cola_logo.png',
-                'bimage/fanta.png',
-                'bimage/disney.png',
-                'bimage/Koroplast_Logo.png',
-                'bimage/Puma_Logo.png',
-                'bimage/Sabon_logo_gold.png',
-                'bimage/_SOCAR.png',
-                'bimage/Sprite_Logo.svg.png',
-                'bimage/Red-Bull-logo-design1-preview.jpg',
-                'bimage/Pepsi_2023.svg.png',
-                'bimage/Ülker_logo.svg.png',
-                'bimage/Eti_logosu.jpg',
-                'bimage/522_torku_logo.jpg',
-                'bimage/A101_logo.svg.png',
-                'bimage/kitapyurdu-793.webp',
-                'bimage/0552e06a-bad1-4f29-b4b5-05d117e4cf04-47448.webp',
-                'bimage/Purina_ONE_logo.png',
-                'bimage/Philips_logo_new.svg.png',
-                'bimage/626_kahvedunyasi.jpg'
+                '/sw.js',
+                '/image1.png',
+                '/bimage/logo.svg',
+                '/bimage/algida.jpg',
+                '/bimage/Coca-Cola_logo.png',
+                '/bimage/fanta.png',
+                '/bimage/disney.png',
+                '/bimage/Koroplast_Logo.png',
+                '/bimage/Puma_Logo.png',
+                '/bimage/Sabon_logo_gold.png',
+                '/bimage/_SOCAR.png',
+                '/bimage/Sprite_Logo.svg.png',
+                '/bimage/Red-Bull-logo-design1-preview.jpg',
+                '/bimage/Pepsi_2023.svg.png',
+                '/bimage/Ülker_logo.svg.png',
+                '/bimage/Eti_logosu.jpg',
+                '/bimage/522_torku_logo.jpg',
+                '/bimage/A101_logo.svg.png',
+                '/bimage/kitapyurdu-793.webp',
+                '/bimage/0552e06a-bad1-4f29-b4b5-05d117e4cf04-47448.webp',
+                '/bimage/Purina_ONE_logo.png',
+                '/bimage/Philips_logo_new.svg.png',
+                '/bimage/626_kahvedunyasi.jpg'
             ]))
     );
 });
 
+// Fetch olayını ele alma
 self.addEventListener('fetch', event => {
     console.log('Service Worker fetching:', event.request.url);
     // Öncelikle önbelleği kontrol edin, eğer önbellekte yoksa ağdan yükleyin
